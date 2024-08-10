8f1f9f26eace71fa5a1f032d08d4d9152fa8983c
Vlad <vladbolkunovv@gmail.com>
2024-08-10 14:51:29 +0300

    feat: uuid validator

--- src/util/validation.ts
+++ src/util/validation.ts
@@ -1,5 +1,6 @@
 import { BadRequest } from "http-errors";
 import Joi from "joi";
+import * as uuid from "uuid";
 
 export function validate<T>(schema: Joi.Schema<T>, data: any) {
   const res = schema.validate(data);
@@ -8,3 +9,10 @@ export function validate<T>(schema: Joi.Schema<T>, data: any) {
   }
   return res.value;
 }
+
+export const uuidValidator = Joi.string()
+  .messages({ uuid: "Invalid UUID" })
+  .custom((value, helpers) => {
+    if (uuid.validate(value)) return value;
+    else return helpers.error("uuid");
+  });
