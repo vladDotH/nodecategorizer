7ea958e3141b3bea8001a810ff620e24f5ff3079
Vlad <vladbolkunovv@gmail.com>
2024-08-10 16:11:25 +0300

    fix(migrations/initial): slug index rm

--- src/migrations/1723236685280_initial.sql
+++ src/migrations/1723236685280_initial.sql
@@ -5,14 +5,13 @@ create table "Categories"
     slug           varchar unique not null,
     name           varchar        not null,
     description    text,
-    "createdDate"    timestamp      not null default current_timestamp,
+    "createdDate"  timestamp      not null default current_timestamp,
     active         bool           not null,
 
     name_ts        tsvector generated always as ( to_tsvector('english', name) ) stored,
     description_ts tsvector generated always as ( to_tsvector('english', description) ) stored
 );
 
-create index "Categories_slug_index" on "Categories" (slug);
 create index "Categories_name_index" on "Categories" using gin (name_ts);
 create index "Categories_description_index" on "Categories" using gin (description_ts);
 create index "Categories_search_index" on "Categories" using gin (name_ts, description_ts);
